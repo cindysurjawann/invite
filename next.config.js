43191e75d7264e2184e2c61f97a41a2fccec8886
cindysurjawann <csurjawan@gmail.com>
2026-06-17 19:43:56 +0700

    fix: deploy error & fix path image

--- next.config.js
+++ next.config.js
@@ -1,14 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/wedding-invitation' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/wedding-invitation/' : '',
-  experimental: {
-    disableOptimizedLoading: true,
-  }
-}
-
-module.exports = nextConfig 
+// Configuration is in next.config.ts
